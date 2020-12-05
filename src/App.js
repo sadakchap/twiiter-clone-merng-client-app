879539d83a1f0a6004b50a8438ac921677b37eb8
sadakchap <aliceprerna@gmail.com>
2020-12-05 23:25:11 +0530

    created Pages & Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
   return (
-      <div>
-        <h2>My first Apollo app 🚀</h2>
-      </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={Home} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
+    </Router>
   );
 }
 
