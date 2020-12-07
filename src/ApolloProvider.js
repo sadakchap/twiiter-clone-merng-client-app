768d9f646e861c2e40969315e14ae07f6376e690
sadakchap <aliceprerna@gmail.com>
2020-12-07 12:19:24 +0530

    Added AUthorization header in apollo-client request

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,13 +1,35 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import App from './App';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import App from "./App";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_API_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("jwt");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_API_URL,
-  cache: new InMemoryCache()
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 export default (
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-)
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
