5b26153c7ce5c855a8103e885ce07c62735171e6
sadakchap <aliceprerna@gmail.com>
2020-12-07 10:12:11 +0530

    conditional rendering for logged In users

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,53 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
-import { Menu } from 'semantic-ui-react'
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+import { AuthContext } from "../context/authContext";
 
 const Navbar = () => {
-    const curPath = window.location.pathname;
-    const path = curPath === '/' ? 'home' : curPath.substr(1);
-    const [activeItem, setActiveItem] = useState(path);
+  const curPath = window.location.pathname;
+  const path = curPath === "/" ? "home" : curPath.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
+  const { user, logout } = useContext(AuthContext);
 
-    const handleItemClick = (e, { name }) => setActiveItem(name);
+  const handleItemClick = (e, { name }) => setActiveItem(name);
 
-    return (
-        <Menu pointing secondary size="massive">
+  return (
+    <Menu pointing secondary size="massive">
+      <Menu.Item
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+      <Menu.Menu position="right">
+        {user ? (
           <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={handleItemClick}
-            as={Link}
-            to="/"
+            name="logout"
+            active={activeItem === "logout"}
+            onClick={logout}
           />
-          <Menu.Menu position='right'>
+        ) : (
+          <>
             <Menu.Item
-                name='login'
-                active={activeItem === 'login'}
-                onClick={handleItemClick}
-                as={Link}
-                to="/login"
+              name="login"
+              active={activeItem === "login"}
+              onClick={handleItemClick}
+              as={Link}
+              to="/login"
             />
             <Menu.Item
-                name='register'
-                active={activeItem === 'register'}
-                onClick={handleItemClick}
-                as={Link}
-                to="/register"
+              name="register"
+              active={activeItem === "register"}
+              onClick={handleItemClick}
+              as={Link}
+              to="/register"
             />
-            <Menu.Item
-                name='logout'
-                active={activeItem === 'logout'}
-                onClick={handleItemClick}
-                as={Link}
-                to="/logout"
-            />
-          </Menu.Menu>
-        </Menu>
-    )
-}
+          </>
+        )}
+      </Menu.Menu>
+    </Menu>
+  );
+};
 
 export default Navbar;
